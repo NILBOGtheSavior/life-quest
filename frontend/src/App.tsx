34e126bec99fa7ea0e4f4ef94e99dead2680f9da
NILBOGtheSavior <naztechmedia@gmail.com>
2025-07-17 15:59:01 -0500

    resolved compilation errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,21 +3,21 @@ import './css/App.css'
 
 // Import modules
 
-import DailyQuests from './modules/daily-quests.tsx'
-import SkillQuests from './modules/skill-quests.tsx'
+import DailyQuests from './modules/daily-quests'
+import SkillQuests from './modules/skill-quests'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div>
+      <div>
         <h1>Life Quest</h1>
-        <div className='panel_container'>
+        <div className='panel_container grid grid-cols-2 md:grid-cols-4 gap-4 p-4 bg-gray-100 min-h-screen'>
           <DailyQuests />
           <SkillQuests />
         </div>
-     </div>
+      </div>
     </>
   )
 }
