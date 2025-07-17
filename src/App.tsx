157ed4f2cbdb80d28a1f2744d9914d3403be997a
NILBOGtheSavior <naztechmedia@gmail.com>
2025-07-16 22:25:39 -0500

    Added quest modules

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Import modules
+
+import DailyQuests from './modules/quest-modules.tsx'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +14,7 @@ function App() {
     <>
      <div>
         <h1>Life Quest</h1>
+        <DailyQuests />
      </div>
     </>
   )
---
+++ src/modules/quest-modules.tsx
@@ -0,0 +1,8 @@
+export default function DailyQuests (){
+  return (
+  <div>
+    <h2>Daily Quests</h2>
+
+  </div>
+  );
+}
