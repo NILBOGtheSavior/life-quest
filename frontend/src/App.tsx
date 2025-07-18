bcd003a4a97a7be4822516cf9efc89d048b9688b
NILBOGtheSavior <naztechmedia@gmail.com>
2025-07-17 19:34:12 -0500

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,10 +13,8 @@ function App() {
     <>
       <div>
         <h1>Life Quest</h1>
-        <div className='panel_container grid grid-cols-2 md:grid-cols-4 gap-4 p-4 bg-gray-100 min-h-screen'>
-          <DailyQuests />
-          <SkillQuests />
-        </div>
+        <DailyQuests />
+        <SkillQuests />
       </div>
     </>
   )
